fa4715589a48fd90a2ed9b1406211f94d028ff73
NagaSivaji <nagasivajisirigineedi@gmail.com>
2023-01-28 10:18:57 +0530

    Added Bootstrap to Project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,5 +5,8 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).use(Antd).mount('#app')
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>HOME</h1>
     <a-button type="primary">Primary Button</a-button>
+    <button class="btn btn-primary">abc</button>
   </div>
 </template>
 
