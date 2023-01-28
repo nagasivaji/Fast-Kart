788f0bd19deb8067439a45be77aa27c32c03ff86
NagaSivaji <nagasivajisirigineedi@gmail.com>
2023-01-28 18:24:43 +0530

    Added fav ico to browser tab

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
     <h1>HOME</h1>
-    <a-button type="primary">Primary Button</a-button>
-    <button class="btn btn-primary">abc</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
